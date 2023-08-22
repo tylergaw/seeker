c513174b637e7bad255ef125e80bd3da821f6983
Tyler Gaw <me@tylergaw.com>
2023-08-22 16:41:17 -0400

    Curated and other Optimizations (#2)
    * Adds valid robots.txt
    * Adds aria label to all items
    * Adds encoding dep to quiet npm warning
    * Memoizes ItemList component to prevent unneeded re-renders
    * Adds posters to curated videos

--- app/components/ItemList/index.tsx
+++ app/components/ItemList/index.tsx
@@ -1,5 +1,7 @@
 import type { FC } from "react";
 import type { Photo, Video } from "pexels";
+
+import { memo } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { random } from "lodash";
@@ -8,12 +10,11 @@ import { getImgDimensionsFromUrl } from "@util/ui";
 import styles from "./style.module.css";
 
 const PhotoGraphic: FC<{ photo: Photo }> = ({ photo }) => {
-  const { width = 300, height = 250 } = getImgDimensionsFromUrl(
-    photo.src.medium,
-  );
+  const src = photo.src.large;
+  const { width = 300, height = 250 } = getImgDimensionsFromUrl(src);
   return (
     <Image
-      src={photo.src.large}
+      src={src}
       alt={photo.alt || ""}
       width={width - random(15, 75)}
       height={height + random(5, 85)}
@@ -25,12 +26,25 @@ const VideoGraphic: FC<{ video: Video }> = ({ video }) => {
   const sdVideos = video.video_files
     .filter((video) => video.quality === "sd")
     .sort((a, b) => (a.width || 1) - (b.width || 2));
-  const thumb = sdVideos[1];
+  const thumb = sdVideos[0];
   const width = (thumb.width || 350) + random(10, 90);
   const height = (thumb.height || 200) - random(7, 45);
+  const poster = new URL(video.image);
+  // NOTE: We wrap width and height in string literal to convert to to string
+  // because it's needed fro searchParams.set
+  poster.searchParams.set("w", `${width}`);
+  poster.searchParams.set("h", `${height}`);
 
   return (
-    <video autoPlay muted loop playsInline width={width} height={height}>
+    <video
+      autoPlay
+      muted
+      loop
+      playsInline
+      width={width}
+      height={height}
+      poster={poster.toString()}
+    >
       <source src={thumb.link} type={thumb.file_type} />
     </video>
   );
@@ -44,7 +58,12 @@ const ItemList: FC<{ items: Array<Photo | Video> }> = ({ items }) => {
         const isPhoto = Object.hasOwn(item, "src");
 
         return (
-          <Link className={styles.item} href={`/${item.id}`} key={item.id}>
+          <Link
+            className={styles.item}
+            href={`/${item.id}`}
+            key={item.id}
+            aria-label={`View item ${item.id}`}
+          >
             {isPhoto && <PhotoGraphic photo={item as Photo} />}
             {isVideo && <VideoGraphic video={item as Video} />}
           </Link>
@@ -54,4 +73,4 @@ const ItemList: FC<{ items: Array<Photo | Video> }> = ({ items }) => {
   );
 };
 
-export default ItemList;
+export default memo(ItemList);
